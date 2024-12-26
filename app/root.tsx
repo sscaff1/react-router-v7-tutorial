8942e30a650175a4819f2af028b862b69ffaca4b
Steven.Scaffidi <Steven.Scaffidi@bottomline.com>
2024-12-26 10:38:14 -0600

    chore: linting

--- app/root.tsx
+++ app/root.tsx
@@ -3,10 +3,10 @@ import {
   Scripts,
   ScrollRestoration,
   isRouteErrorResponse,
-} from "react-router";
-import type { Route } from "./+types/root";
+} from 'react-router';
+import type { Route } from './+types/root';
 
-import appStylesHref from "./app.css?url";
+import appStylesHref from './app.css?url';
 
 export default function App() {
   return (
@@ -66,15 +66,15 @@ export function Layout({ children }: { children: React.ReactNode }) {
 // The top most error boundary for the app, rendered when your app throws an error
 // For more information, see https://reactrouter.com/start/framework/route-module#errorboundary
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
+  let message = 'Oops!';
+  let details = 'An unexpected error occurred.';
   let stack: string | undefined;
 
   if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
+    message = error.status === 404 ? '404' : 'Error';
     details =
       error.status === 404
-        ? "The requested page could not be found."
+        ? 'The requested page could not be found.'
         : error.statusText || details;
   } else if (import.meta.env.DEV && error && error instanceof Error) {
     details = error.message;
